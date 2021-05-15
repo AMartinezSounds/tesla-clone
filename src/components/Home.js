5450a217428f38fe75559daf5293d382b8676461
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-15 22:03:33 +0200

    Tesla cloe boilerplate

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Home() {
+    return (
+        <Container>
+            Home
+        </Container>
+    )
+}
+
+const Container = styled.div`
+    height: 100vh;
+`;
+
+export default Home
