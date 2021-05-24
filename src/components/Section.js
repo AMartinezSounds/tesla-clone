050c4bf241e8abcc2bd6f363f4839d55ee061fa9
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-24 19:26:46 +0200

    Model S section done

---
+++ src/components/Section.js
@@ -0,0 +1,82 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+    return (
+        <Wrap>
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        Custom Order
+                    </LeftButton>
+                    <RightButton>
+                        Existing Inventory
+                    </RightButton>
+                </ButtonGroup>
+                <DownArrow src="images/down-arrow.svg" />
+            </Buttons>
+            
+        </Wrap>
+    )
+}
+
+export default Section;
+
+const Wrap = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: url('images/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`;
+
+const Buttons = styled.div`
+
+`;
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, .8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: .85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+    margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: .65;
+    color: black;
+`;
+
+const DownArrow = styled.img`
+    /* margin-top: 20px; */
+    height: 40px;
+    animation: animateDown infinite 1.5s;
+`;
