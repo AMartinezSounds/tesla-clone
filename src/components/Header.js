ef767d0b66255845d40a64ebceec1ac767b2429b
Alfredo Martínez <alfre_canos13@icloud.com>
2021-05-26 16:35:51 +0200

    project finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,22 @@ import React, { useState } from 'react'
 import styled from 'styled-components';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
-    const [burgerStatus, setBurgerStatus] = useState(false)
+    const [burgerStatus, setBurgerStatus] = useState(false);
+    const cars = useSelector(selectCars);
     return (
         <Container>
             <a href="#">
                 <img src="/images/logo.svg" alt="" />
             </a>
             <Menu>
-                <a href="#">Model S</a>
-                <a href="#">Model 3</a>
-                <a href="#">Model X</a>
-                <a href="#">Model Y</a>
+                {cars && cars.map((car,index) => (
+                   <a key={index} href="#">{car}</a>
+
+                ))}
             </Menu>
             <RightMenu>
                 <a href="#">Shop</a>
@@ -25,6 +28,9 @@ function Header() {
                 <CloseWrapper>
                     <CustomClose onClick={() => setBurgerStatus(false)}/>
                 </CloseWrapper>
+                {cars && cars.map((car,index) => (
+                   <li key={index}><a href="#">{car}</a></li>
+                ))}
                 <li><a href="#">Existing Inventory</a></li>
                 <li><a href="#">Used Inventory</a></li>
                 <li><a href="#">Trade-in</a></li>
@@ -95,6 +101,7 @@ const BurgerNav = styled.div`
     flex-direction: column;
     text-align: start;
     transform: ${props => props.show ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.2s ease-in;
     li {
         padding: 15px 0;
         border-bottom: 1px solid rgba(0, 0, 0, .2);
